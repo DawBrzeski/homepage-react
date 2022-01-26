f4006af46083a641d08eebf91e41f33a78c846b3
Dawid Brzeski <daw.brzeski@gmail.com>
2022-01-26 11:40:13 +0100

    color text updated to fix contrast ratio

--- src/core/App/theme.js
+++ src/core/App/theme.js
@@ -10,7 +10,7 @@ const colorNames = {
     whiteLilac: "#FBFBFE",
     porcelain: "#F2F3F4",
     mercury: "#e5e5e5",
-    slateGray: "#6E7E91",
+    slateGray: "#262626",
     doveGray: "#6D6D6D",
     tundora: "#414141",
     mineShaft: "#252525",
